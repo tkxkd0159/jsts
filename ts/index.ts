4f23b75fe7cd428a0a660e7da4eaa751aff30101
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-05-23 01:26:48 +0900

    ts module test

--- ts/index.ts
+++ ts/index.ts
@@ -1,10 +1,18 @@
+import defaultMyfunc, { studentNumber, myName } from "./modules/my-module";
+
+
 let message: string = 'Hello World';
-const mine = "3.11"
-var mine2 = "3.567"
-let mine3 = "4.2"
+const mine = "3.11";
+var mine2 = "3.567";
+let mine3 = "4.2";
 
 
-document.body.innerHTML = message
-document.body.innerHTML = mine
-document.body.innerHTML = mine2
-document.body.innerHTML = mine3
+// document.body.innerHTML = message;
+// document.body.innerHTML = mine;
+// document.body.innerHTML = mine2;
+// document.body.innerHTML = mine3;
+
+console.log(studentNumber);
+console.log(myName);
+console.log(defaultMyfunc);
+console.log("NPM test");
---
+++ ts/modules/my-module.ts
@@ -0,0 +1,5 @@
+const studentNumber: number = 201902000
+let myName: string = "LJS"
+
+export default function myDfunc(x: number): number { return x * x * x }
+export { studentNumber, myName }

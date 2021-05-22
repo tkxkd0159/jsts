caeedab5efbb2fa552a7b6dd141b2ee7fb07f049
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-05-22 19:12:43 +0900

    Add debug options

---
+++ ts/debug/index.js
@@ -0,0 +1,9 @@
+let message = 'Hello World';
+const mine = "3.11";
+var mine2 = "3.567";
+let mine3 = "4.2";
+document.body.innerHTML = message;
+document.body.innerHTML = mine;
+document.body.innerHTML = mine2;
+document.body.innerHTML = mine3;
+//# sourceMappingURL=index.js.map
--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,10 @@
 let message: string = 'Hello World';
-const mine = 3.11
-var mine2 = 3.567
-let mine3 = 4.2
-console.log(message);
+const mine = "3.11"
+var mine2 = "3.567"
+let mine3 = "4.2"
+
+
+document.body.innerHTML = message
+document.body.innerHTML = mine
+document.body.innerHTML = mine2
+document.body.innerHTML = mine3

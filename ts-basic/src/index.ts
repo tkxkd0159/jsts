fb97b2a7241fc01ab460ce00cbd2de3aa9b888ce
Jaeseung Lee(KU) <41176085+tkxkd0159@users.noreply.github.com>
2021-08-04 21:24:29 +0900

    Practice class

--- ts-basic/src/index.ts
+++ ts-basic/src/index.ts
@@ -1,8 +1,7 @@
-import defaultMyfunc, { studentNumber, myName } from "./modules/types";
+import defaultMyfunc, { studentNumber, myName, Student, ICompany, IPerson } from "./modules/types";
 
 
 console.log(studentNumber);
 console.log(myName);
 console.log(defaultMyfunc);
 console.log(`My env is ${process.env.MY_WIN_PATH}`);
-
--- ts-basic/src/modules/types.ts
+++ ts-basic/src/modules/types.ts
@@ -9,6 +9,50 @@ let myName: string = "LJS"
  */
  function myDfunc(x: number): number { return x * x * x }
 
+ interface ICompany {
+     name: string
+     field: string
+     wage: number
+ }
+ interface IPerson {
+    name: string
+    age?: number
+}
+interface IKorean extends IPerson{
+    area: string
+    country: string
+}
+
+abstract class Korean implements IKorean {
+    abstract name: string
+    abstract age?: number
+    abstract area: string
+    abstract country: string
+
+    abstract savedVal(): string
+    testPrint() {
+        console.log("Test Inheritance")
+    }
+}
+
+class Student  extends Korean {
+    name: string
+    country: string
+    age?: number
+    area: string
+    static initVal = 1
+    constructor(name: string, country: string, area: string, age?: number) {
+        super()
+        this.name = name;
+        this.age = age;
+        this.country = country
+        this.area = area
+    }
+
+    savedVal() {
+        return `${this.name} ${this.country} ${this.area}`
+    }
+}
 
 export default myDfunc
-export { studentNumber, myName }
+export { studentNumber, myName, Student, ICompany, IPerson }

55ed8642a6d849dbc5ad45e3783fc4a249cbf566
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-04-30 00:28:22 +0900

    Update .gitignore
    Add .idea/
    
    Update .gitignore

--- simple_block/src/env.config.ts
+++ simple_block/src/env.config.ts
@@ -2,6 +2,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    UserBaseUrl: process.env.USER_SERVICE_URL ?? '',
-    EtlUrl: process.env.ETL_SERVICE_URL ?? ''
+    UserBaseUrl: process.env.USER_SERVICE_URL ?? 'N/A',
+    EtlUrl: process.env.ETL_SERVICE_URL ?? 'N/A'
    }

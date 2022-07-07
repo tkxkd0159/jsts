3e781861c17885819be6b564b7b5e3992f663e0c
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2022-07-08 05:35:22 +0900

    feat: add simple blockchain ds to practice

--- simple_block/src/index.ts
+++ simple_block/src/index.ts
@@ -1,6 +1,65 @@
-import ev from "./env.config"
+import {createHash} from "crypto";
+import ev from "./env.config";
 
-console.log(ev.UserBaseUrl)
-let a = 30;
-let b = 40;
-console.log(a, b)
+import {init, init2} from './mypkg'
+
+console.log(init({url: "sample", debug: true}))
+console.log(init2({url: "sample", debug: true}))
+
+
+class Block {
+    protected hash: string;
+    constructor(
+        protected prevHash: string,
+        protected height: number,
+        protected data: string,
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data)
+    }
+
+    static calculateHash(prevHash: string, height: number, data: string): string {
+        const source = `${prevHash}${height}${data}`
+        return createHash("sha256").update(source).digest("hex")
+    }
+
+    getPrevHash(): string {
+        return this.prevHash
+    }
+    getHash(): string {
+        return this.hash
+    }
+    getHeight(): number {
+        return this.height
+    }
+    getData(): string {
+        return this.data
+    }
+}
+
+class Blockchain {
+    private blocks: Block[]
+    constructor(
+        private genesisSource: string,
+        private genesisData: string
+    ) {
+        this.blocks = [];
+
+        const genesisHash = createHash("sha256").update(genesisSource).digest("hex");
+        const genesisBlock = new Block(genesisHash, 0, genesisData)
+        this.blocks.push(genesisBlock)
+
+    }
+
+    addBlock(blk: Block) {
+        this.blocks.push(blk)
+    }
+
+    getBlocks(): Block[] {
+        return [...this.blocks] // do not allow to modify 'blocks' property
+    }
+}
+
+const myChain = new Blockchain("genesis", "This is first block")
+console.log(myChain)
+
+const blks = myChain.getBlocks()
---
+++ simple_block/src/mypkg.js
@@ -0,0 +1,30 @@
+
+
+// @ts-check
+/**
+ *
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config) {
+    return true
+}
+
+/**
+ * @typedef Config
+ * @type {object}
+ * @property {boolean} debug - Is debug
+ * @property {string} url
+ */
+
+// @ts-check
+/**
+ *
+ * @param {Config} config
+ * @returns {boolean}
+ */
+ export function init2(config) {
+    return false
+}
